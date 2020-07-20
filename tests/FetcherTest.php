72bbe9f2f385826080dceb91e31da3c1b1cd0170
Raf <peliraph@gmail.com>
2020-07-20 12:29:23 +0200

    select as

---
+++ tests/FetcherTest.php
@@ -0,0 +1,57 @@
+<?php
+
+use Fetcher\BaseFetcher;
+use Fetcher\Field\FieldType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * User: Raphael Pelissier
+ * Date: 20-07-20
+ * Time: 11:56
+ */
+
+class FetcherTest extends TestCase
+{
+    /**
+     * @var BaseFetcher
+     */
+    private $userFetcher;
+
+    protected function setUp(): void
+    {
+        // @UserFetcher
+        $this->userFetcher = new class extends BaseFetcher {
+            protected $table = 'user';
+
+            protected function getFields(): array
+            {
+                return [
+                    'id' => FieldType::INT,
+                    'username' => FieldType::STRING
+                ];
+            }
+
+            protected function getJoins(): array
+            {
+                return [];
+            }
+        };
+
+        parent::setUp();
+    }
+
+    public function testValidSelectAs()
+    {
+        $this->assertInstanceOf(
+            BaseFetcher::class,
+            $this->userFetcher->select(['username AS name'])
+        );
+    }
+
+    public function testInvalidSelectAs()
+    {
+        $this->expectException(Exception::class);
+
+        $this->userFetcher->select(['user_name AS name']);
+    }
+}
